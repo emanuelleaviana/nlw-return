6892667fc49eb7c9c06b178bc14919f4cbc0c628
Emanuelle Viana <manuavbr@gmail.com>
2022-05-08 19:46:09 -0300

    up stage 05 b.b

--- ProjetoNLW/main.js
+++ ProjetoNLW/main.js
@@ -4,6 +4,29 @@ onScroll()
 function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+    activateMenuAtCurrentSection()
+}
+
+function activateMenuAtCurrentSection() {
+    //Linha alvo
+    const targetLine = scrollY + innerHeight / 2
+
+    //o topo da seção
+    const sectionTop = services.offsetTop
+
+    //a altura da seção
+    const sectionHeight = home.offsetHeight
+    
+    //o topo da seção chegou ou ultrapassou a linha alto
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+    //informacoes
+    console.log('O topo da seção chegou o passou da linha?' , sectionTopReachOrPassedTargetline)
+
+    //verificar se a base está abaixo da linha alvo
+    c
+
+
 }
 
 function showNavOnScroll() {
